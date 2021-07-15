056a2009fb82dee039894b5e9b09dcc4b5e933e6
Gunin <gunin2001@gmail.com>
2021-07-15 15:00:19 +0530

    2nd commit

--- app/src/main/java/com/example/camerax/MainActivity.kt
+++ app/src/main/java/com/example/camerax/MainActivity.kt
@@ -3,8 +3,14 @@ package com.example.camerax
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.camera.core.impl.PreviewConfig
+import android.view.View
+import android.view.ViewGroup
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
 import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 import java.security.Permission
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +28,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
-        val previewConfig = PreviewConfig.Builder()
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetAspectRatio(AspectRatio.RATIO_16_9)
+        }.build()
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener{
+            var parent = textureView.parent as ViewGroup
+            parent.removeView(textureView)
+            parent.addView(textureView,0)
+            textureView.surfaceTexture = it.surfaceTexture
+        }
+        CameraX.bindToLifecycle(this,preview)
     }
 }
